09fb375442b603f7ff38335c6098cd77ef1100da
nekolauyiu <genspark_dev@genspark.ai>
2026-04-15 07:43:00 +0000

    feat: add cat favicon

--- src/index.tsx
+++ src/index.tsx
@@ -41,6 +41,7 @@ function shell(title: string, active: string, body: string, script = '') {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/png" href="/static/favicon.png">
   <title>${title} · neko</title>
   <style>
     ${BASE_CSS}
